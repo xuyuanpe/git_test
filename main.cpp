e98e44f06cf4ff3de8cb631b932d01fb34bc8a0b
windows_xyp2 <2924706952@qq.com>
2026-02-04 15:06:20 +0800

    在新建分支中修改文件并推送到远程仓库

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ int sum(int x, int y)
 }
 int main(int argc,char* argv[])
 {
-    int a =10;
-    int b =20;
+    //int a =10;
+    //int b =20;
     //int ret =sum(a,b);
 	//vector<int>v2={6,7,8,9,10};
     //vector<int>v={1,2,3,4,5};//ubuntu_xyp
