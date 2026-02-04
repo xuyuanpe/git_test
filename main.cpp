441806cf3e93be17ab4bcda79bcfbfd08122b0d5
ubuntu_xyp <1035724241@qq.com>
2026-02-03 17:38:09 -0800

    修改了main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 using namespace std;
+int sum(int x, int y)
+{
+    return x+y;
+}
 int main(int argc,char* argv[])
 {
-    
+    int a =10;
+    int b =20;
+    int ret =sum(a,b);
     return 0;
 }
