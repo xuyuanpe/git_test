d504a5114ca40ce4f94e0b3ae4976510a96d7bfe
windows_xyp2 <2924706952@qq.com>
2026-02-04 13:40:51 +0800

    创建了向量vector

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int sum(int x, int y)
 {
@@ -9,5 +10,6 @@ int main(int argc,char* argv[])
     int a =10;
     int b =20;
     //int ret =sum(a,b);
+	vector<int>v2={6,7,8,9,10};
     return 0;
 }
